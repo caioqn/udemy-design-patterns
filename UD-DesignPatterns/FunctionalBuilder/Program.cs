0671b76a22bfb6fcc710ed4115f7dc16802bf84b
Caio <79511540+caioqn@users.noreply.github.com>
2022-12-27 17:10:53 -0300

    Functional Builder
    Adiciona exemplo com functional builder

---
+++ UD-DesignPatterns/FunctionalBuilder/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace FunctionalBuilder
+{
+    public class Person
+    {
+        public string Name, Position;
+    }
+
+    public abstract class FunctionalBuilder<TSubject, TSelf> 
+        where TSelf : FunctionalBuilder<TSubject,TSelf>
+        where TSubject : new()
+    {
+        private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+
+        public TSelf Do(Action<Person> action) => AddAction(action);
+
+        public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+
+        private TSelf AddAction(Action<Person> action)
+        {
+            actions.Add(p => { action(p); return p; });
+            return (TSelf) this;
+        }
+    }
+
+    public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>
+    {
+        public PersonBuilder Called(string name) => Do(p => p.Name = name);
+    }
+
+    public static class PersonBuilderExtensions
+    {
+        public static PersonBuilder WorksAs(this PersonBuilder builder, string position) => builder.Do(p => p.Position = position);
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonBuilder()
+                .Called("Jao")
+                .WorksAs("Developer")
+                .Build();
+        }
+    }
+}
