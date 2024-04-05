539a126332290826b957cab07fe4071cd41c652f
caioqn <79511540+caioqn@users.noreply.github.com>
2024-04-05 00:03:48 -0300

    UPDATE FluentBuilderInheritance
    Adiciona o caso problemático e comentários.

--- UD-DesignPatterns/FluentBuilderInheritance/Program.cs
+++ UD-DesignPatterns/FluentBuilderInheritance/Program.cs
@@ -3,13 +3,15 @@ namespace FluentBuilderInheritance
 {
     public class Person
     {
-        public string Name;
+        public string? Name;
 
-        public string Position;
+        public string? Position;
 
+        #region allows the fluent interface to work
         public class Builder : PersonJobBuilder<Builder> { }
 
-        public static Builder New => new Builder();
+        public static Builder New => new();
+        #endregion
 
         public override string ToString()
         {
@@ -17,6 +19,32 @@ namespace FluentBuilderInheritance
         }
     }
 
+
+    #region fluent builder with inheritance problem
+    /*
+    public class PersonInfoBuilder
+    {
+        protected Person person = new();
+
+        public PersonInfoBuilder Called(string name)
+        {
+            person.Name = name;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonInfoBuilder
+    {
+        public PersonJobBuilder WorksAsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+    }
+    */
+    #endregion
+
+    #region fluent builder with design to solve the problem and use inheritance
     public abstract class PersonBuilder
     {
         protected Person person = new Person();
@@ -28,32 +56,41 @@ namespace FluentBuilderInheritance
     }
 
     // class Foo : Bar<Foo> 
-    public class PersonInfoBuilder<SELF> 
-        : PersonBuilder 
+    public class PersonInfoBuilder<SELF>
+        : PersonBuilder
         where SELF : PersonInfoBuilder<SELF>
     {
         public SELF Called(string name)
         {
             person.Name = name;
-            return (SELF) this;
+            return (SELF)this;
         }
     }
 
-    public class PersonJobBuilder<SELF> 
-        : PersonInfoBuilder<PersonJobBuilder<SELF>> 
+    public class PersonJobBuilder<SELF>
+        : PersonInfoBuilder<PersonJobBuilder<SELF>>
         where SELF : PersonJobBuilder<SELF>
     {
-        public SELF WorksAsA(string position) 
+        public SELF WorksAsA(string position)
         {
             person.Position = position;
-            return (SELF) this;
+            return (SELF)this;
         }
     }
+    #endregion
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            /* Problematic Builder
+            var pb = new PersonJobBuilder();
+            var badme = pb
+                .Called("caio")
+                .WorksAsA("asdf"); // doesnt Work
+            */
+
+            // Working Builder
             var me = Person.New
                 .Called("Caio")
                 .WorksAsA("asdf")
