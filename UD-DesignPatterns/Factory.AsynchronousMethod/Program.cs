bc1865e570b8420fc36a2b5f3c4dfc7e51ec5e9d
caioqn <79511540+caioqn@users.noreply.github.com>
2024-05-20 01:32:38 -0300

    ADD Factory Method and Async Method Version

---
+++ UD-DesignPatterns/Factory.AsynchronousMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Factory.AsynchronousMethod
+{
+    public class Foo
+    {
+        private Foo() { }
+
+        private async Task<Foo> InitAsyn()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsyn();
+        }
+    }
+
+
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            Foo x = await Foo.CreateAsync();
+        }
+    }
+}
---
+++ UD-DesignPatterns/Factory.Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace Factory.Method
+{
+    public class Point
+    {
+        // factory methods
+        public static Point NewCartesianPoint(double x, double y) => new(x, y);
+
+        public static Point NewPolarPoint(double rho, double theta) => new(rho * Math.Cos(theta), rho * Math.Sin(theta));
+
+        public double x { get; private set; }
+        public double y { get; private set; }
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string? ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            Console.WriteLine(point);
+        }
+    }
+}
