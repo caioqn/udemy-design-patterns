713fa11b0bbe7e1d0659a68d39bd9acec93db893
caioqn <79511540+caioqn@users.noreply.github.com>
2024-05-21 23:21:40 -0300

    UPDATE Factory.Method Pattern
    Adiciona classe para armazenar os métodos do factory pattern

--- UD-DesignPatterns/Factory.Method/Program.cs
+++ UD-DesignPatterns/Factory.Method/Program.cs
@@ -2,17 +2,27 @@
 
 namespace Factory.Method
 {
-    public class Point
+    public static class PointFactory
     {
-        // factory methods
+        // factory methods inside a factory class
         public static Point NewCartesianPoint(double x, double y) => new(x, y);
 
         public static Point NewPolarPoint(double rho, double theta) => new(rho * Math.Cos(theta), rho * Math.Sin(theta));
+    }
+
+    public class Point
+    {
+        /* factory methods inside the class
+        public static Point NewCartesianPoint(double x, double y) => new(x, y);
+
+        public static Point NewPolarPoint(double rho, double theta) => new(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        */
 
         public double x { get; private set; }
         public double y { get; private set; }
 
-        private Point(double x, double y)
+        // To use an factory class, the constructor needs to be public
+        public Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -28,7 +38,7 @@ namespace Factory.Method
     {
         static void Main(string[] args)
         {
-            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
             Console.WriteLine(point);
         }
     }
